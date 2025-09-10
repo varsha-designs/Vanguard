24ed221bd786efa0f614980a677adb94a814bdd0
varsha <varshabharathi.642@gmail.com>
2025-09-10 23:31:11 +0530

    Added the history blade

---
+++ resources/views/files/history.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.sidebar')
+
+@section('page-title', 'File History')
+
+@section('content')
+<div class="max-w-4xl mx-auto bg-white shadow-lg rounded-lg p-6">
+    <h2 class="text-2xl font-bold mb-4">Uploaded Files History</h2>
+
+    <ul class="divide-y divide-gray-200">
+        @forelse($files as $file)
+            <li class="py-3 flex justify-between items-center hover:bg-gray-50 px-3 rounded transition">
+                <span class="text-gray-700 font-medium">{{ $file->file_name }}</span>
+                <span class="text-gray-500 text-sm">{{ $file->created_at->format('d M Y H:i') }}</span>
+            </li>
+        @empty
+            <li class="py-2 text-gray-500">No files uploaded yet.</li>
+        @endforelse
+    </ul>
+</div>
+@endsection
