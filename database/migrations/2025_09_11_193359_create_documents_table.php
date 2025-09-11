9e7935bc6327b6f1208cae3df59a5d97491f0505
varsha <varshabharathi.642@gmail.com>
2025-09-11 23:08:53 +0530

    Created the documents table

---
+++ database/migrations/2025_09_11_193359_create_documents_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('student_id'); // link to student
+            $table->string('file_name'); // e.g., ID, Aadhaar, or custom name
+            $table->string('upload_file'); // file upload path
+            $table->timestamps();
+
+            // Foreign key constraint
+            $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documents');
+    }
+};
