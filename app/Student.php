ffc3c8c5a25cf11477b1c8bf323b64f16b9bf0f9
varsha <varshabharathi.642@gmail.com>
2025-09-11 23:04:47 +0530

    Created student model

---
+++ app/Student.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Vanguard;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Vanguard\Document;
+
+class Student extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'full_name',
+        'email',
+        'whatsapp_number',
+        'dob',
+        'gender',
+        'address',
+        'college',
+        'degree',
+        'year_of_passing',
+        'company',
+        'role',
+        'experience',
+    ];
+    public function documents()
+{
+    return $this->hasMany(Document::class);
+
+}
+
+}
