b6f3978f572dce9a1b8ca0797846c25b2d1fc143
varsha <varshabharathi.642@gmail.com>
2025-09-17 17:02:22 +0530

    Added the new button in index

--- resources/views/daily_activities/index.blade.php
+++ resources/views/daily_activities/index.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
 <div class="max-w-7xl mx-auto p-6">
-    <h1 class="text-3xl font-extrabold mb-6 text-gray-900">Daily Activities</h1>
+    <h1 class="text-3xl font-extrabold mb-6 text-gray-900 ">Daily Activities</h1>
+    <a href="{{ route('daily_activities.create') }}" class="btn btn-primary mb-2">New</a>
 
     @if(session('success'))
         <div class="bg-green-300 border border-green-300 text-green-800 px-4 py-3 rounded-md mb-6 shadow-md">
@@ -42,7 +43,7 @@
                         <td>
     <a href="{{ route('daily_activities.show', $activity) }}" class="btn btn-info btn-sm">View</a>
     <a href="{{ route('daily_activities.edit', $activity->id) }}" class="btn btn-warning btn-sm">Edit</a>
-    <form action="{{ route('daily_activities.destroy', $activity) }}" method="POST" style="display:inline"
+    <form action="{{ route('daily_activities.destroy', $activity->id) }}" method="POST" style="display:inline"
           onsubmit="return confirm('Are you sure you want to delete this activity?')">
         @csrf
         @method('DELETE')
