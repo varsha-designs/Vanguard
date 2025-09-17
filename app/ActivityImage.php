709c303c6bc3caf9e356b68db7e6e4b2c8a499a7
varsha <varshabharathi.642@gmail.com>
2025-09-17 17:16:47 +0530

    Created the model ActivityImage

---
+++ app/ActivityImage.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Vanguard;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Vanguard\DailyActivity;
+
+class ActivityImage extends Model
+{
+    use HasFactory;
+
+     protected $fillable = ['activity_id', 'image_path'];
+
+    public function activity() {
+        return $this->belongsTo(DailyActivity::class);
+    }
+}
