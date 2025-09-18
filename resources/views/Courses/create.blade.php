6626526a30c3d96a40fff7c6a7fffa535da6586d
varsha <varshabharathi.642@gmail.com>
2025-09-18 21:19:19 +0530

    Replaced from time to date field

--- resources/views/Courses/create.blade.php
+++ resources/views/Courses/create.blade.php
@@ -51,6 +51,18 @@
                value="{{ old('course_fee') }}" required>
     </div>
 
+           <div class="mb-3">
+    <label for="start_date" class="form-label">Start Date</label>
+    <input type="date" name="start_date" id="start_date" class="form-control" value="{{ old('start_date') }}" required>
+</div>
+
+<div class="mb-3">
+    <label for="end_date" class="form-label">End Date</label>
+    <input type="date" name="end_date" id="end_date" class="form-control" value="{{ old('end_date') }}" required>
+</div>
+
+
+
 
         <button type="submit" class="btn btn-primary">Save Course</button>
         <a href="{{ route('courses.index') }}" class="btn btn-secondary">Cancel</a>
