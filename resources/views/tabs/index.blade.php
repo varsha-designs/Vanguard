cd3219b01798916096ad1ac8eef46001a17e4aec
varsha <varshabharathi.642@gmail.com>
2025-09-24 16:55:49 +0530

    Added the button for id card

--- resources/views/tabs/index.blade.php
+++ resources/views/tabs/index.blade.php
@@ -3,7 +3,15 @@
 
 @section('content')
 <div class="p-6">
+     <div class="flex justify-between items-center mb-6">
+        <h1 class="text-2xl font-bold">Student Details</h1>
+        <a href="{{ route('tabs.id-card') }}"
+    class="px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-500 transition">
+    ID Card
+</a>
 
+    </div>
+     <script src="https://cdn.tailwindcss.com"></script>
     <!-- Tabs Navigation -->
     <div class="border-b border-gray-200">
         <nav class="-mb-px flex space-x-8">
@@ -23,6 +31,8 @@
                class="whitespace-nowrap py-4 px-1 border-b-2 text-sm font-medium {{ request()->is('tabs/photos') ? 'border-indigo-600 text-indigo-600' : 'border-transparent text-gray-500 hover:text-gray-700 ' }}">
                 Photos
             </a>
+
+
         </nav>
     </div>
 
@@ -31,4 +41,5 @@
         @yield('tab-content')
     </div>
 </div>
+
 @endsection
